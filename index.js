1edc84208a4a64366aa4facb7240f2800b539cfc
roman-akhundlu <romanakhundlu@gmail.com>
2024-05-10 00:56:49 +0400

    birinci layihe (mudafie ucun)

---
+++ index.js
@@ -0,0 +1,22 @@
+    const swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }
+    });
