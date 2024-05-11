e934b74b0627ca132709e31b27e3ca305a5f8077
roman-akhundlu <romanakhundlu@gmail.com>
2024-05-11 11:52:20 +0400

    AI layihe (Mudafie ucun)1

--- index.js
+++ index.js
@@ -20,3 +20,32 @@
         },
       }
     });
+
+
+
+
+
+
+
+    const roman = new Swiper(".yourSwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }
+    });
